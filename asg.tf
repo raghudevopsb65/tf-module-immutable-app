ebe8bc40b2824d7d0a569ca415576f30a90ca6a5
Raghu K <raghuk.vit@gmail.com>
2022-08-10 07:16:50 +0530

    Update terraform/tf-module-immutable-app/

--- asg.tf
+++ asg.tf
@@ -4,6 +4,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.ASG_MAX
   min_size            = var.ASG_MIN
   vpc_zone_identifier = var.PRIVATE_SUBNET_IDS
+  target_group_arns   = [aws_lb_target_group.target-group.arn]
 
   launch_template {
     id      = aws_launch_template.launch-template.id
